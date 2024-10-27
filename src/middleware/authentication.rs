833e2fabc9bfea72c752283c8e130e897bd59b4d
Riley Moura <nmoura49@gmail.com>
2024-10-27 16:09:56 +0000

    Fixed authentication

--- src/middleware/authentication.rs
+++ src/middleware/authentication.rs
@@ -1,7 +1,8 @@
 use jsonwebtoken::{decode, DecodingKey, Validation, TokenData};
 use rocket::http::Status;
 use rocket::async_trait;  // Ensure this is included if you're using async
-use rocket::request::{FromRequest, Request};
+use rocket::request::{FromRequest, Outcome, Request};
+//use rocket::route::Outcome;
 use rocket::serde::json::Json;
 use serde_json::json;
 use crate::models::user_model::Claims;
@@ -18,30 +19,34 @@ pub struct AuthToken(pub Claims);
 #[async_trait]
 impl<'r> FromRequest<'r> for AuthToken {
     type Error = Json<serde_json::Value>;
-
-    async fn from_request(request: &'r Request<'_>) -> Result<Self, Self::Error> {
+    
+    async fn from_request(request: &'r Request<'_>) -> Outcome<Self, Self::Error> {
         let secret: &[u8] = b"your_secret_key_here"; // Replace with your actual secret key
 
         if let Some(token) = request.headers().get_one("Authorization") {
             if let Some(token) = token.strip_prefix("Bearer ") {
                 match verify_token(token, secret) {
-                    Ok(claims) => Ok(AuthToken(claims.claims)),
-                    Err(_) => Err(Json(json!( {
-                        "error": "Unauthorized",
-                        "message": "Invalid token."
-                    }))),
+                    Ok(claims) => Outcome::Success(AuthToken(claims.claims)),
+                    Err(_) => {
+                        Outcome::Error((Status::Unauthorized, Json(json!( {
+                            "error": "Unauthorized",
+                            "message": "Invalid token."
+                        }))))
+                    },
                 }
             } else {
-                Err(Json(json!( {
+                Outcome::Error((Status::BadRequest, Json(json!( {
                     "error": "Bad Request",
                     "message": "Invalid token format."
-                })))
+                }))))
             }
         } else {
-            Err(Json(json!( {
+            Outcome::Error((Status::Unauthorized, Json(json!( {
                 "error": "Unauthorized",
                 "message": "No token provided."
-            })))
+            }))))
         }
     }
+
+    
 }
